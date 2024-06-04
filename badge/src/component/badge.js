6225012db04455409f9cc6fbf57eb431233ab87f
Poonam Gahatraj <poonamgtrz@gmail.com>
2024-06-04 10:55:45 +0545

    badge

--- badge/src/component/badge.js
+++ badge/src/component/badge.js
@@ -2,12 +2,17 @@ import { useState } from "react";
 import './badge.css'
 
 export default function Badge (){
-const[count,setCount]=useState(2)
+const[count,setCount]=useState(2);
+
+function showIncrement(){
+    setCount(count+1)
+}
 
     return(
         <>
         <button style={{position:"relative",marginTop:"1%"}}>Notifications</button>
         {count > 0 && <div className="badge">{count}</div>}
+        <button onClick={showIncrement}>+</button>
         </>
     )
 }
